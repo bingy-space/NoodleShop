352d006da8e6c5827ead4f3c4a0c9bfd85b422d6
BingNa Yang <bing334@gmail.com>
2022-08-09 19:59:46 -0400

    create method to subscribe and assign the data to product array

--- noodle-shop-frontend/src/app/components/product-list/product-list.component.ts
+++ noodle-shop-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  // Set Property
+  products: Product[] = [];
 
-  constructor() { }
+  // Inject ProductService
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.listProducts();
   }
 
+  listProducts(){
+    // Method is invoked once you 'subscribe'
+    this.productService.getProductList().subscribe(
+      data => {
+        // Assign results data to the Product array
+        this.products = data;
+        console.log(this.products)
+      }
+    )
+  }
 }
