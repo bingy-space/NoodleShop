735d291f92b623bb94aca976b80b2c48fd3adcce
BingNa Yang <bing334@gmail.com>
2022-08-09 20:11:25 -0400

    add @CrossOrgin to accept web browser scripts

--- noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/ProductRepository.java
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.noodleshop.noodleshopbackend.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.noodleshop.noodleshopbackend.entity.Product;
 
+// Accept calls from web browser scripts from this origin
+// Orgin is: protocol + hostname + port
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
