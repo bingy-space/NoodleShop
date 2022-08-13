3dadb89ce52fca5136a03911f5387c24dc08ba26
BingNa Yang <bing334@gmail.com>
2022-08-13 19:28:34 -0400

    Update ProductRepository to add new method: findByCategoryId

--- noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/ProductRepository.java
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.noodleshop.noodleshopbackend.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.noodleshop.noodleshopbackend.entity.Product;
@@ -9,5 +12,10 @@ import com.noodleshop.noodleshopbackend.entity.Product;
 // Orgin is: protocol + hostname + port
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
-
+	
+	// Spring Data REST automatically expose endpoint
+	// 		http://localhost:8080/api/products/search/findByCategoryId?id={number}
+	// Behind the scenes, Spring will execute a query similar to this: 
+	// 		SELECT * FROM product WHERE category_id=?
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
