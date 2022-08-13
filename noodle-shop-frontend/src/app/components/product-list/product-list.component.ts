9432e299331d305976756c6b0c890da2a904ad7a
BingNa Yang <bing334@gmail.com>
2022-08-12 20:33:32 -0400

    enhance productList component, get category id and pass to product service

--- noodle-shop-frontend/src/app/components/product-list/product-list.component.ts
+++ noodle-shop-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,17 +11,41 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
   // Set Property
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
   // Inject ProductService
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts(){
+    // Check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      // Get the "id" param string
+      // Convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }else{
+      // No category id available, default to category "id" 1
+      this.currentCategoryId = 1;
+    }
+
     // Method is invoked once you 'subscribe'
-    this.productService.getProductList().subscribe(
+    // this.productService.getProductList().subscribe(
+    //   data => {
+    //     // Assign results data to the Product array
+    //     this.products = data;
+    //     console.log(this.products)
+    //   }
+    // )
+
+    // Now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         // Assign results data to the Product array
         this.products = data;
