a0492f187be5fb6779534e4f32c941c6e60075eb
BingNa Yang <bing334@gmail.com>
2022-08-15 18:26:21 -0400

    Create method to get product categories
    - Update `GetResponse` interface name
    - Create interface for ProductCategory
    - Create service method to get product categories
    - Set categoryUrl

--- noodle-shop-frontend/src/app/services/product.service.ts
+++ noodle-shop-frontend/src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,12 @@ import { Product } from '../common/product';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
   getAllProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );
   }
@@ -22,15 +24,27 @@ export class ProductService {
     // Need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
-  
-}
 
-interface GetResponse{
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+
+}
+// Unwraps the JSON from Spring Data REST _embedded entry
+interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory{
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
