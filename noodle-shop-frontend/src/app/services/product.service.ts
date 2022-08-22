121e6cf4d62e6bb72f48c3892c56967100f81985
BingNa Yang <bing334@gmail.com>
2022-08-22 18:15:41 -0400

    Add Pagination support to Product Service
    - refactor the interface for GetResponseProducts

--- noodle-shop-frontend/src/app/services/product.service.ts
+++ noodle-shop-frontend/src/app/services/product.service.ts
@@ -20,6 +20,13 @@ export class ProductService {
     );
   }
 
+  getProductListPaginate(thePage: number, thePageSize: number,theCategoryId: number ): Observable<GetResponseProducts>{
+    // Need to build URL based on category id, page and size
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`+`&page=${thePage}&size=${thePageSize}`;
+    
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   getProductList(theCategoryId: number ): Observable<Product[]>{
     // Need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
@@ -60,6 +67,12 @@ export class ProductService {
 interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  },
+  page:{
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
