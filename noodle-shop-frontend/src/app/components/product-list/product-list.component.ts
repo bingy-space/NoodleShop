dccb1051d0d94016939cffe11a9c1402dd34f329
BingNa Yang <bing334@gmail.com>
2022-08-28 13:58:24 -0400

    update ProductListComponent for setting page size

--- noodle-shop-frontend/src/app/components/product-list/product-list.component.ts
+++ noodle-shop-frontend/src/app/components/product-list/product-list.component.ts
@@ -99,4 +99,10 @@ export class ProductListComponent implements OnInit {
     )
 
   }
+
+  updatePageSize(pageSize: string){
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
 }
