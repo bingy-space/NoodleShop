c26dde59f773838684b29fb40ad4324832a854de
BingNa Yang <bing334@gmail.com>
2022-09-06 19:53:22 -0400

    call addToCart CartSerice

--- noodle-shop-frontend/src/app/components/product-list/product-list.component.ts
+++ noodle-shop-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -23,7 +25,7 @@ export class ProductListComponent implements OnInit {
   previousKeyword: string = "";
 
   // Inject ProductService
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -115,6 +117,9 @@ export class ProductListComponent implements OnInit {
   }
 
   addToCart(theProduct: Product){
-    console.log(theProduct);
+    console.log(`Adding to car: ${theProduct.name}, ${theProduct.unitPrice}`)
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
   }
 }
