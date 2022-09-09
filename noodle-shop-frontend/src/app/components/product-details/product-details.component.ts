9d511c8e3709186ed2f58ed82fa07fd44c9e08fa
BingNa Yang <bing334@gmail.com>
2022-09-08 21:31:48 -0400

    create addToCart method to handle click handler

--- noodle-shop-frontend/src/app/components/product-details/product-details.component.ts
+++ noodle-shop-frontend/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,7 +15,7 @@ export class ProductDetailsComponent implements OnInit {
   // The ! is the not-null assertion operator. Tell TypeScript compiler to suspend strict null and undefined checks for a property
   product!: Product;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() =>{
@@ -30,6 +32,11 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     )
+  }
+
+  addToCart(){
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 
 }
