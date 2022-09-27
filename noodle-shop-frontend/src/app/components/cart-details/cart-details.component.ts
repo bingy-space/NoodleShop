87fa97c7a5b5512fb0fc86e5def570bc96495dfe
BingNa Yang <bing334@gmail.com>
2022-09-27 18:36:16 -0400

    create “increment” click handler method

--- noodle-shop-frontend/src/app/components/cart-details/cart-details.component.ts
+++ noodle-shop-frontend/src/app/components/cart-details/cart-details.component.ts
@@ -30,4 +30,8 @@ export class CartDetailsComponent implements OnInit {
     this.cartService.computeCartTotal();
   }
 
+  incrementQuantity(theCartItem: CartItem){
+    this.cartService.addToCart(theCartItem);
+  }
+
 }
