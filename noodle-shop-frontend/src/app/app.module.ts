184aa38b5ac9b121f4c30c5f60f9f01dd1f684fd
BingNa Yang <bing334@gmail.com>
2022-10-01 21:19:35 -0400

    generate checkout component

--- noodle-shop-frontend/src/app/app.module.ts
+++ noodle-shop-frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 // Define routes
 const routes: Routes = [
@@ -33,7 +34,8 @@ const routes: Routes = [
     SearchComponent,
     ProductDetailsComponent,
     CartStatusComponent,
-    CartDetailsComponent
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
