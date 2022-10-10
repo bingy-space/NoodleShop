73a281d3e230c5526d74ee863528bf14062aceeb
BingNa Yang <bing334@gmail.com>
2022-10-10 13:52:44 -0400

    add methods to the form service for months and year

--- noodle-shop-frontend/src/app/services/shop-form.service.ts
+++ noodle-shop-frontend/src/app/services/shop-form.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,34 @@ import { Injectable } from '@angular/core';
 export class ShopFormService {
 
   constructor() { }
+
+  getCreditCardMonths(startMonth: number): Observable<number[]>{
+    let data: number[] = [];
+
+    // Build an array for "Month" dropdown list
+    // Start at current month and loop until
+
+    for(let theMonth = startMonth; theMonth <= 12; theMonth++){
+      data.push(theMonth);
+    }
+
+    // The "of" operator from rxjs will wrap an object as an Observable
+    // rxjs: Reactive JS
+    return of(data);
+  }
+
+  getCreditCardYears(): Observable<number[]>{
+    let data: number[] = [];
+
+    // Build an array for "Year" dropdown list
+    // Start at current year and loop for next 10 years
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for(let theYear = startYear; theYear <= endYear; theYear){
+      data.push(theYear);
+    }
+
+    return of(data);
+  }
 }
