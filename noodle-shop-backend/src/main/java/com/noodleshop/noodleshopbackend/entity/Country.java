6c9891c5f3989ad3df174c48400fe832f78bdfa2
BingNa Yang <bing334@gmail.com>
2022-10-11 20:39:56 -0400

    create Country JPA entity

---
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/entity/Country.java
@@ -0,0 +1,36 @@
+package com.noodleshop.noodleshopbackend.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+public class Country {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="code")
+	private String code;
+	
+	@Column(name="name")
+	private String name;
+	
+	@OneToMany(mappedBy = "country")
+	private List<State> states;
+	
+}
