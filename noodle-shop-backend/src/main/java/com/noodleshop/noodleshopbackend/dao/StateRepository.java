9fd9dd6780cb24b7c331e286263ab355c887369a
BingNa Yang <bing334@gmail.com>
2022-10-13 20:26:42 -0400

    create StateRepository interface

---
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/StateRepository.java
@@ -0,0 +1,20 @@
+package com.noodleshop.noodleshopbackend.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.noodleshop.noodleshopbackend.entity.State;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface StateRepository extends JpaRepository<State, Integer>{
+	
+	// To retrieve states for a given country code
+	// http://localhost:8080/api/states/search/findByCountryCode?code=IN
+	List<State> findByCountryCode(@Param("code") String code);
+
+}
