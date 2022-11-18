c4d8d9f284585509469e12b99d8d8ca1339cfe73
BingNa Yang <bing334@gmail.com>
2022-11-17 20:42:48 -0500

    setup placeOrder method
    Retrieve the order info from dto
    Generate tracking number
    Populate order with orderItems
    Populate order with billingAddress and shippingAddress
    Populate customer with order
    Save to the database
    Return a response

--- noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/service/CheckoutServiceImpl.java
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/service/CheckoutServiceImpl.java
@@ -1,11 +1,20 @@
 package com.noodleshop.noodleshopbackend.service;
 
+import java.util.Set;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.noodleshop.noodleshopbackend.dao.CustomerRepository;
 import com.noodleshop.noodleshopbackend.dto.Purchase;
 import com.noodleshop.noodleshopbackend.dto.PurchaseResponse;
+import com.noodleshop.noodleshopbackend.entity.Customer;
+import com.noodleshop.noodleshopbackend.entity.Order;
+import com.noodleshop.noodleshopbackend.entity.OrderItem;
 
+@Service
 public class CheckoutServiceImpl implements CheckoutService{
 	
 	private CustomerRepository customerRepository;
@@ -16,9 +25,39 @@ public class CheckoutServiceImpl implements CheckoutService{
 	}
 
 	@Override
+	@Transactional
 	public PurchaseResponse placeOrder(Purchase purchase) {
-		// TODO Auto-generated method stub
-		return null;
+		// Retrieve the order info from dto
+		Order order = purchase.getOrder();		
+				
+		// Generate tracking number
+		String orderTrackingNumber = generateOrderTrackingNumber();
+		order.setOrderTrackingNumber(orderTrackingNumber);
+		
+		// Populate order with orderItems
+		Set<OrderItem> orderItems = purchase.getOrderItems();
+		orderItems.forEach(item -> order.add(item));
+		
+		// Populate order with billingAddress and shippingAddress
+		order.setBillingAddress(purchase.getBillingAddress());		
+		order.setShippingAddress(purchase.getShippingAddress());
+		
+		// Populate customer with order
+		Customer customer = purchase.getCustomer();
+		customer.add(order);
+		
+		// Save to the database
+		customerRepository.save(customer);
+		
+		// Return a response
+		return new PurchaseResponse(orderTrackingNumber);
+	}
+
+	private String generateOrderTrackingNumber() {
+		// Generate a random UUID number (UUID version-4)
+		// For details see: https://en.wikipedia.org/wiki/Universally_unique_identifier	
+		
+		return UUID.randomUUID().toString();
 	}
 
 }
