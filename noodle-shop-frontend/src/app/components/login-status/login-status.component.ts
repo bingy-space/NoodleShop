396ce83febb44a58d2daebdf79bb5553947f7f91
BingNa Yang <bing334@gmail.com>
2022-11-29 19:52:15 -0500

    setup loginStatus component, create getUserDetails method

--- noodle-shop-frontend/src/app/components/login-status/login-status.component.ts
+++ noodle-shop-frontend/src/app/components/login-status/login-status.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-login-status',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginStatusComponent implements OnInit {
 
-  constructor() { }
+  isAuthenticated: boolean = false;
+  userFullName: string = '';
+
+  constructor(private oktaAuthService: OktaAuthStateService, @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
   ngOnInit(): void {
+    // Subscribe to authentication state change
+    this.oktaAuthService.authState$.subscribe((result) => {
+      this.isAuthenticated = result.isAuthenticated!;
+      this.getUserDetails();
+    })
   }
+
+  getUserDetails(){
+    if(this.isAuthenticated){
+      // Fetch the logged in user details(user's claim)
+      // User full name is exposed as a property name
+      this.oktaAuth.getUser().then((res) => {
+        this.userFullName = res.name as string;
+      })
+    }
+  }
+
+  
 
 }
