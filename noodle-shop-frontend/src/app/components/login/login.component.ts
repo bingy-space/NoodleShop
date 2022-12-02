08f8497a87fd9d0a503566e511c5fc973eccc64d
BingNa Yang <bing334@gmail.com>
2022-12-01 21:01:42 -0500

    fix route typo

--- noodle-shop-frontend/src/app/components/login/login.component.ts
+++ noodle-shop-frontend/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
-import appConfig from 'src/app/config/app-config';
+import appConfig from '../../config/app-config';
 import OktaSignIn from '@okta/okta-signin-widget';
 
 @Component({
