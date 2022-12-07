ef08501464cdb2360ffd2bca7e22876995989438
BingNa Yang <bing334@gmail.com>
2022-12-06 21:06:57 -0500

    Add new method to CustomerRepository

--- noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/CustomerRepository.java
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/CustomerRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.noodleshop.noodleshopbackend.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
-
+	// Behind the scenes, Spring will execute a query similar to this:
+	// SELECT * FROM Customer c WHERE c.email = theEmail
+	// Method returns null if not found
+	Customer findByEmail(String theEmail);
 }
