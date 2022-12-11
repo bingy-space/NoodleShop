1a7616d62b3df74800827cab312807a0a83aeabf
BingNa Yang <bing334@gmail.com>
2022-12-10 19:34:59 -0500

    Modify Spring Data REST Detection Strategy

--- noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/ProductRepository.java
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.noodleshop.noodleshopbackend.entity.Product;
@@ -11,6 +12,7 @@ import com.noodleshop.noodleshopbackend.entity.Product;
 // Accept calls from web browser scripts from this origin
 // Orgin is: protocol + hostname + port
 //@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	
 	// Spring Data REST automatically expose endpoint
