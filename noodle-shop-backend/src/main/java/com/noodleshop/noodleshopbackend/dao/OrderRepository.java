7d2d4dcd7d73dcf3aa123d638ae04518c39bc660
BingNa Yang <bing334@gmail.com>
2022-12-11 15:43:53 -0500

    Create OrderRepository REST API

--- noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/OrderRepository.java
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/OrderRepository.java
@@ -1,5 +1,19 @@
 package com.noodleshop.noodleshopbackend.dao;
 
-public interface OrderRepository {
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.noodleshop.noodleshopbackend.entity.Order;
+
+@RepositoryRestResource
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	// Spring will execute a query similar to this:
+	// SELECT * FROM orders
+	// LEFT OUTER JOIN customer
+	// ON orders.customer_id = customer.id
+	// WHERE customer.email = :email
+	Page<Order> findByCustomerEmail(@Param("email") String email, Pageable pageable);
 }
