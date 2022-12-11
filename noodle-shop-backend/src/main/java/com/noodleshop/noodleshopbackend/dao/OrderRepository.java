b6a9c0ddad535be4077c0cfdd8076eaf26042544
BingNa Yang <bing334@gmail.com>
2022-12-11 18:55:34 -0500

    fix Pageable import

--- noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/OrderRepository.java
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/dao/OrderRepository.java
@@ -1,7 +1,7 @@
 package com.noodleshop.noodleshopbackend.dao;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
