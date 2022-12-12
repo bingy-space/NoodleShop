76123487f3ce9cb331f42910e53d043256827d03
BingNa Yang <bing334@gmail.com>
2022-12-12 18:16:01 -0500

    Keep track of logged in user’s email with web browser storage

--- noodle-shop-frontend/src/app/components/login-status/login-status.component.ts
+++ noodle-shop-frontend/src/app/components/login-status/login-status.component.ts
@@ -12,6 +12,8 @@ export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
   userFullName: string = '';
 
+  storage: Storage = sessionStorage;
+
   constructor(private oktaAuthService: OktaAuthStateService, @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
   ngOnInit(): void {
@@ -28,6 +30,11 @@ export class LoginStatusComponent implements OnInit {
       // User full name is exposed as a property name
       this.oktaAuth.getUser().then((res) => {
         this.userFullName = res.name as string;
+
+        // Retrieve the user's email from authentication response
+        const theEmail = res.email;
+        // Store the email in browser storage
+        this.storage.setItem('userEmail', JSON.stringify(theEmail));
       })
     }
   }
