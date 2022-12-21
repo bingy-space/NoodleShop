0515402f3f7c5d5ba241b0d6fc43bc3d3cbd6cc2
BingNa Yang <bing334@gmail.com>
2022-12-20 21:23:11 -0500

    fix HTTP Checkout POST by disabling CSRF

--- noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/config/SecurityConfiguration.java
+++ noodle-shop-backend/src/main/java/com/noodleshop/noodleshopbackend/config/SecurityConfiguration.java
@@ -29,6 +29,8 @@ public class SecurityConfiguration {
 		// Force a non-empty response body for 401's to make the response more friendly
 		Okta.configureResourceServer401ResponseBody(http);
 		
+		// Disable CSRF since we are not using Cookies for session tracking
+		http.csrf().disable();
 		
 		return http.build();
 	}
