9805fc78c229c3fa69264cce3a3e80630a072759
BingNa Yang <bing334@gmail.com>
2022-12-24 19:07:07 -0500

    Use environment

--- noodle-shop-frontend/src/app/services/auth-interceptor.service.ts
+++ noodle-shop-frontend/src/app/services/auth-interceptor.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@angular/core';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
 import { from, lastValueFrom, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,8 @@ export class AuthInterceptorService implements HttpInterceptor{
 
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>>{
     // Only add an access token for secured endpoints
-    const securedEndPoints = ['http://localhost:8080/api/orders'];
+    const theEndPoint = environment.noodleShopApiUrl + '/orders';
+    const securedEndPoints = [theEndPoint];
 
     if(securedEndPoints.some(url => request.urlWithParams.includes(url))){
       // Get access token
--- noodle-shop-frontend/src/app/services/product.service.ts
+++ noodle-shop-frontend/src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Product } from '../common/product';
 import { ProductCategory } from '../common/product-category';
 
@@ -9,8 +10,8 @@ import { ProductCategory } from '../common/product-category';
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products';
-  private categoryUrl = 'http://localhost:8080/api/product-category';
+  private baseUrl = environment.noodleShopApiUrl + '/products';
+  private categoryUrl = environment.noodleShopApiUrl + '/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
