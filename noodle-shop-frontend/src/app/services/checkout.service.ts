a5a5854bbe00fa3118525f67c03a41660ead961e
BingNa Yang <bing334@gmail.com>
2023-01-01 14:27:06 -0500

    create a PaymentIntent and load Stripe JavaScript

--- noodle-shop-frontend/src/app/services/checkout.service.ts
+++ noodle-shop-frontend/src/app/services/checkout.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 import { Purchase } from '../common/purchase';
 
 @Injectable({
@@ -10,10 +11,15 @@ import { Purchase } from '../common/purchase';
 export class CheckoutService {
 
   private purchaseUrl = environment.noodleShopApiUrl + '/checkout/purchase';
+  private paymentIntentUrl = environment.noodleShopApiUrl + '/checkout/payment-intent';
 
   constructor(private httpClient: HttpClient) { }
 
   placeOrder(purchase: Purchase): Observable<any>{
     return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+
+  createPaymentIntent(paymentInfo: PaymentInfo): Observable<any> {
+    return this.httpClient.post<PaymentInfo>(this.paymentIntentUrl, paymentInfo);
   }
 }
